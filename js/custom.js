1388b920057d8855e2485a900a40ccf38b3f377e
James Anthony Dollente <james-anthony.dollente@transcosmos.com.ph>
2021-09-01 13:25:58 +0800

    Header Desktop and Mobile

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,29 @@
-console.log("HELLO JS!")
+const btnHamburger = document.querySelector(".hamburger");
+const menuLink = document.querySelector(".menu");
+
+// Window screen
+window,
+  addEventListener("resize", function () {
+    if (window.innerWidth <= 767) {
+      menuLink.classList.add("collapse");
+    } else if (window.innerWidth >= 768) {
+      btnHamburger.classList.remove("open");
+      menuLink.classList.remove("collapse");
+      menuLink.classList.remove("show");
+    }
+  });
+
+
+btnHamburger.addEventListener("click", function () {
+  console.log("click");
+
+  if (btnHamburger.classList.contains("open")) {
+    //open burger
+    btnHamburger.classList.remove("open");
+    menuLink.classList.remove("show");
+  } else {
+    //close burger
+    btnHamburger.classList.add("open");
+    menuLink.classList.add("show");
+  }
+});
