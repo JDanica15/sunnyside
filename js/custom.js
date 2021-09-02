68a604a2c835b2419308b7ab74e731b8467541c5
James Anthony Dollente <james-anthony.dollente@transcosmos.com.ph>
2021-09-02 15:38:53 +0800

    footer class

--- js/custom.js
+++ js/custom.js
@@ -68,3 +68,23 @@ const handleScrollAnimation = () => {
 window.addEventListener("scroll", () => {
   handleScrollAnimation();
 });
+
+// SCROLL PERCENTAGE
+const updateScrollPercentage = function() { 
+  const heightOfWindow = window.innerHeight,
+    contentScrolled = window.pageYOffset,
+    bodyHeight = document.body.offsetHeight,
+    percentage = document.querySelector("[data-scrollPercentage] .percentage")
+
+  if(bodyHeight - contentScrolled <= heightOfWindow) {
+   percentage.style.width = "100%"
+  }
+  else {
+    const total = bodyHeight - heightOfWindow,
+      got = contentScrolled,
+      percent = parseInt((got/total) * 100)
+      percentage.style.width = percent + "%"
+  }
+}
+
+window.addEventListener('scroll', updateScrollPercentage)
